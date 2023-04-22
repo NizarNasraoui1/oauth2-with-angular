7f70031a2dce64bce4f97d5b82c2b5c9bb7fcd52
Nizar <nizar.al.nasraoui@gmail.com>
2023-04-22 11:13:04 +0200

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { GoogleApiService, UserInfo } from './google-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-google-oauth-example';
+
+  mailSnippets: string[] = []
+  userInfo?: UserInfo
+
+  constructor(private readonly googleApi: GoogleApiService) {
+    googleApi.userProfileSubject.subscribe( info => {
+      this.userInfo = info
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn()
+  }
+
+  logout() {
+    this.googleApi.signOut()
+  }
+
+  async getEmails() {
+    if (!this.userInfo) {
+      return;
+    }
+
+    const userId = this.userInfo?.info.sub as string
+    const messages = await lastValueFrom(this.googleApi.emails(userId))
+    messages.messages.forEach( (element: any) => {
+      const mail = lastValueFrom(this.googleApi.getMail(userId, element.id))
+      mail.then( mail => {
+        this.mailSnippets.push(mail.snippet)
+      })
+    });
+  }
+}
